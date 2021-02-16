e5934a2d18e1c67436b14354750e3ec89f02f90b
shem <shemmwaka1@gmail.com>
2021-02-16 16:37:10 +0300

    login ui

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,2 +1,3 @@
-<div class="d-flex align-items-center py-3"><img class="mr-2 rounded-circle" src="/img/qubes.png" alt="" width="40" /><span class="font-sans-serif">Qubes ERP</span>
+<div class="d-flex align-items-center py-3"><img class="mr-2 rounded-circle" src="/img/qubes.png" alt="" width="40" /><span class="font-sans-serif">
+    <h1 class="text-5xl text-blue-500 font-bold">Qubes Ict</h1></span>
 </div>
--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,8 +1,9 @@
 <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
     <div>
-        <div class="d-flex align-items-center py-3"><img class="rounded-full" src="/img/qubes.png" alt="" width="100" />
+        <div class="d-flex align-items-center py-3"><img class="rounded-full" src="/img/qubes.png" alt="" width="120" />
         </div>
-        Qubes Accounting
+        
+    <h3 class="text-2xl text-blue-500 font-bold">Qubes Accounting</h3>
     </div>
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
