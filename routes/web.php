8f5b9e43edfbfa87b89aa1701ad8931632f36a00
shem <shemmwaka1@gmail.com>
2021-02-16 16:21:44 +0300

    UI links & redesign

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ use Laravel\Jetstream\Rules\Role;
 
 Route::get('/',[LoansController::class,'index'])->middleware('auth')->middleware('auth');
 
+Route::get('/loans', [LoansController::class,'index'])->middleware('auth');
+
 Route::get('/loans/add', [LoansController::class,'create'])->middleware('auth');
 Route::any('/loans/{id}/repayments/create', [RepaymentsController::class,'create'])->middleware('auth');
 Route::post('/loans/{id}/repayments', [RepaymentsController::class,'store'])->middleware('auth');
